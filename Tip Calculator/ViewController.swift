c60dbe5fc31259b0dbc21f4daece4e1a02771342
Taylor Ngo <cs.taylorngo@gmail.com>
2021-06-09 23:56:08 -0400

    final commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -3,17 +3,58 @@
 //  Tip Calculator
 //
 //  Created by Taylor Ngo on 6/9/21.
+//  112626118
+//  taylor.ngo@stonybrook.edu
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        billInput.delegate = self
     }
-
-
+    
+    @IBAction func fifteenPercentTip(_ sender: Any) {
+        if let bill = Double(billInput.text!){
+            let total = round((bill * 1.15) * 100) / 100
+            let tipAmount = round((bill * 0.15) * 100) / 100
+            tipAmountLabel.text = "Tip Amount: $" + String(tipAmount)
+            totalLabel.text = "Total: $" + String(total)
+        }
+        else{
+            return
+        }
+    }
+    
+    @IBAction func eighteenPercentTip(_ sender: Any) {
+        if let bill = Double(billInput.text!){
+            let total = round((bill * 1.18) * 100) / 100
+            let tipAmount = round((bill * 0.18) * 100) / 100
+            tipAmountLabel.text = "Tip Amount: $" + String(tipAmount)
+            totalLabel.text = "Total: $" + String(total)
+        }
+        else{
+            return
+        }
+    }
+    
+    @IBAction func twentyPercentTip(_ sender: Any) {
+        if let bill = Double(billInput.text!){
+            let total = round((bill * 1.20) * 100) / 100
+            let tipAmount = round((bill * 0.20) * 100) / 100
+            tipAmountLabel.text = "Tip Amount: $" + String(tipAmount)
+            totalLabel.text = "Total: $" + String(total)
+        }
+        else{
+            return
+        }
+    }
+    
+    
 }
 
